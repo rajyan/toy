915b8d3307a2babfabe018bcb4ec11db7191bede
rajyan <kitakita7617@gmail.com>
2021-01-21 13:22:45 +0900

    created random test

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 #include "../library/src/RandNum.cpp"
 #include "../library/src/timer.cpp"
 
-std::string random_string(const std::size_t& max_len) {
-    RandNum ran;
+std::string random_string(const std::size_t &max_len, RandNum &ran) {
     std::size_t size = ran(1, max_len);
     std::string res(size, 0);
     std::generate(res.begin(), res.end(), [&ran]() { return 'a' + ran(std::numeric_limits<long long>::max()) % 26; });
@@ -16,45 +15,44 @@ std::string random_string(const std::size_t& max_len) {
 
 int main() {
 
-    constexpr std::size_t len_max = 30;
-    constexpr std::size_t vec_size = 100;
+    constexpr std::size_t key_max = 30;
+    constexpr std::size_t init_size = 500;
     constexpr std::size_t test_size = 1000000;
-    constexpr std::size_t hash_num = 3;
-    constexpr std::size_t table_size = 500;
+    constexpr std::size_t table_size = 10000;
+    constexpr std::size_t hash_num = (std::size_t)(0.7 * table_size / init_size + 0.5);
 
-    std::vector<std::string> vec(vec_size);
-    for (std::size_t i = 0; i < vec_size; i++) {
-        vec[i] = random_string(len_max);
-    }
-
+    RandNum ran;
     std::set<std::string> st;
     BloomFilter bf(hash_num, table_size);
-    RandNum ran;
+    std::vector<std::string> vec(init_size);
+    for (std::size_t i = 0; i < init_size; i++) {
+        std::string key = random_string(key_max, ran);
+        st.emplace(key);
+        bf.add(key);
+    }
+
+    // assert BloomFilter contains the initial values
+    for (const auto &key : st) {
+        assert(bf.contains(key));
+    }
 
     std::size_t fp_count = 0;
-    std::size_t contains_count = 0;
     for (int i = 0; i < test_size; i++) {
-        std::size_t index = ran(0, vec_size - 1);
-        if (ran(0, 1)) {
-            st.emplace(vec[index / 2]);
-            bf.add(vec[index / 2]);
-        }
-        else {
-            contains_count++;
-            bool st_exists = st.find(vec[index]) != st.end();
-            bool bf_exists = bf.contains(vec[index]);
 
-            // no false negative
-            assert(!(st_exists && !bf_exists));
+        std::string key = random_string(key_max, ran);
+        bool st_exists = st.find(key) != st.end();
+        bool bf_exists = bf.contains(key);
 
-            if (!st_exists && bf_exists) {
-                std::cout << "false positive with " << vec[index] << '\n';
-                fp_count++;
-            }
+        // no false negative
+        assert(!(st_exists && !bf_exists));
+
+        if (!st_exists && bf_exists) {
+            std::cout << "false positive with " << key << '\n';
+            fp_count++;
         }
     }
 
-    std::cout << "false positive rate: " << 1.0 * fp_count / contains_count << '\n';
+    std::cout << "false positive rate: " << 1.0 * fp_count / test_size << '\n';
 
     return 0;
 }
